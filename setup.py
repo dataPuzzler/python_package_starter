1345c3298ce6ed355c08111821ca1a885c9e27bc
= <=>
2021-05-02 16:08:36 +0200

    Eding sample_pck as  editable dev dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
-    name="Sample Package",
+    name="sample_pck",
     install_requires=[
         "numpy",
         "requests",
