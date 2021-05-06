2acc9cba6870691c5212890e52e13bde318d9e38
= <=>
2021-05-06 21:10:01 +0200

    The active package VERSION should be set in VERSION via CI

--- src/sample_pck/__init__.py
+++ src/sample_pck/__init__.py
@@ -1,1 +1,7 @@
-__version__ = "1.0.0"
+from pathlib import Path 
+
+repo_dir = Path(__file__).parent.parent.parent
+__repo_dir__ = repo_dir
+
+# read from file manipulated by CI Pipeline
+__version__ =  repo_dir.joinpath('VERSION').open().read().strip()
